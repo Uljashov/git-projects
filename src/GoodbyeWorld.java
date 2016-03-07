496263f26dbd03589ffb2fb5b1526ccf54dbb580
Uljashov <ya.ivanuljashov@gmail.com>
2016-03-07 13:19:42 +0300

    сделал метод "Goodbye"

--- src/GoodbyeWorld.java
+++ src/GoodbyeWorld.java
@@ -2,7 +2,9 @@
  * Created by Ivan on 07.03.2016.
  */
 public class GoodbyeWorld {
-    public static void main(String[] args) {
-        System.out.println("Goodbye World!");
-    }
+
+        public static void Gododbye() {
+            System.out.println("Goodbye World!");
+        }
+
 }
--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -5,5 +5,6 @@ public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello my friends!");
+        GoodbyeWorld.Gododbye();
     }
 }
